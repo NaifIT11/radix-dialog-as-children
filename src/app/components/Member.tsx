800291cf0c4346968f3a8c685a7a7dfa644bfebc
Naif Otibi <nn.n@outlook.cl>
2024-10-26 04:30:52 +0300

    adding dialog.close

--- src/app/components/Member.tsx
+++ src/app/components/Member.tsx
@@ -1,5 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Edit2 } from "lucide-react";
+import { Edit2, X } from "lucide-react";
 
 type Member = {
   name: string;
@@ -22,15 +22,23 @@ export default function Member({ name, role, email }: Member) {
             <Dialog.Overlay className="fixed inset-0 bg-black/55" />
             <Dialog.Content className="fixed flex flex-col gap-4 rounded p-4 bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
               <div className="space-y-2">
-                <Dialog.Title className="text-xl">
-                  Edit Information
-                </Dialog.Title>
+                <div className="flex items-center justify-between">
+                  <Dialog.Title className="text-xl">
+                    Edit Information
+                  </Dialog.Title>
+                  <Dialog.Close>
+                    <X className="w-4 h-4" />
+                  </Dialog.Close>
+                </div>
                 <Dialog.DialogDescription className="text-sm">
                   by editing this information u accept our term and condition
                 </Dialog.DialogDescription>
               </div>
-              <form className="flex flex-col gap-3" onSubmit={(e: React.FormEvent<HTMLFormElement>) => {e.preventDefault()}}>
-                
+              <form className="flex flex-col gap-3">
+                <div className="grid gap-2">
+                  <label htmlFor="name">name:</label>
+                  <input type="text" id="name" />
+                </div>
               </form>
             </Dialog.Content>
           </Dialog.Portal>
